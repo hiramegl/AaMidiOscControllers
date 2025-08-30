4d6c7e7f228113c9db82a2da61a43be7cfd9798b
Hiram Galicia <hiramegl@yahoo.com>
2025-08-30 09:47:49 +0200

    Adding focus navigation in seq mode, removing warp and adding crop function in Seq mode

--- AaTouch/ClipNav.py
+++ AaTouch/ClipNav.py
@@ -13,7 +13,8 @@ class ClipNav(Base):
   # ********************************************************
 
   def connect(self):
-    self.reg_idx_cb('clip/nav', 4, self.on_clip_nav)
+    self.reg_idx_cb('clip/nav'      , 4, self.on_clip_nav)
+    self.reg_idx_cb('clip/focus_nav', 4, self.on_clip_focus_nav)
 
   # ********************************************************
 
@@ -27,19 +28,69 @@ class ClipNav(Base):
       if nTrackIdxAbs > 0:
         oTrackOrReturn = self.get_track_or_return(nTrackIdxAbs - 1)
         self.sel_track(oTrackOrReturn)
+        self.alert('Select track to the left')
+      else:
+        self.alert('Cannot select track to the left')
 
     elif sDir == 'RIGHT':
       if nTrackIdxAbs < len(self.tracks_and_returns()):
         oTrackOrReturn = self.get_track_or_return(nTrackIdxAbs + 1)
         self.sel_track(oTrackOrReturn)
+        self.alert('Selecting track to the right')
+      else:
+        self.alert('Cannot select track to the right')
 
     elif sDir == 'DOWN':
       if nTrackIdxAbs < len(self.scenes()):
         oScene = self.get_scene(nSceneIdxAbs + 1)
         self.sel_scene(oScene)
+        self.alert('Selecting next scene')
+      else:
+        self.alert('Cannot select next scene')
 
     elif sDir == 'UP':
       if nSceneIdxAbs > 0:
         oScene = self.get_scene(nSceneIdxAbs - 1)
         self.sel_scene(oScene)
+        self.alert('Selecting previous scene')
+      else:
+        self.alert('Cannot select previous scene')
+
+  def on_clip_focus_nav(self, plSegs, plMsg):
+    nIdx = int(plSegs[3])
+    sDir = self.m_lDirs[nIdx]
+    nColOff = self.col_offset()
+    nRowOff = self.row_offset()
+
+    if sDir == 'LEFT':
+      if nColOff > 0:
+        nColOff -= 1
+        self.state().set_clip_offsets(nColOff, nRowOff)
+        self.alert('Focused track to the left')
+      else:
+        self.alert('Cannot focus track to the left')
+
+    elif sDir == 'RIGHT':
+      if nColOff < len(self.tracks_and_returns()):
+        nColOff += 1
+        self.state().set_clip_offsets(nColOff, nRowOff)
+        self.alert('Focusing track to the right')
+      else:
+        self.alert('Cannot focus track to the right')
+
+    elif sDir == 'DOWN':
+      if nColOff < len(self.scenes()):
+        nRowOff += 1
+        self.state().set_clip_offsets(nColOff, nRowOff)
+        self.alert('Focusing next scene')
+      else:
+        self.alert('Cannot focus next scene')
+
+    elif sDir == 'UP':
+      if nRowOff > 0:
+        nRowOff -= 1
+        self.state().set_clip_offsets(nColOff, nRowOff)
+        self.alert('Focusing previous scene')
+      else:
+        self.alert('Cannot focus previous scene')
 
